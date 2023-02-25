6704ed2a4092045b6fef180004385988baae866f
Vancho Dimitrov aka Stoyanov <vancho.dimitrov@gmail.com>
2023-02-25 23:05:03 +0100

    bug fix.

--- TwitterCloneClient.Tests/Controllers/CityControllerTests.cs
+++ TwitterCloneClient.Tests/Controllers/CityControllerTests.cs
@@ -9,12 +9,13 @@ namespace TwitterCloneClient.Tests.Controllers
     public class CitiesControllerTests
     {
         private readonly DataContext _dataContext;
-        private readonly string _connectionString = "Server=(localdb)\\mssqllocaldb;Database=TestDatabase;Trusted_Connection=True;MultipleActiveResultSets=true";
 
         /// <summary>
         /// Use this constructor if testing conducted with a real MSSQL DB.
         /// </summary>
         #region MSSQLDBTesting
+        // private readonly string _connectionString = "Server=(localdb)\\mssqllocaldb;Database=TestDatabase;Trusted_Connection=True;MultipleActiveResultSets=true";
+
         //public CitiesControllerTests()
         //{
         //    var options = new DbContextOptionsBuilder<DataContext>()
